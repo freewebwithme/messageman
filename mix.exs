466902d500aacec13919a121df425b472e9ec5fa
Taehwan Kim <wlminimal@gmail.com>
2018-08-28 16:43:55 -0700

    Change deps

--- mix.exs
+++ mix.exs
@@ -25,7 +25,7 @@ defmodule Messageman.Mixfile do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       # {:ex_twilio, [path: "../ex_twilio"]},
-      {:ex_twilio, github: "wlminimal/ex_twilio"}
+      {:ex_twilio, github: "wlminimal/ex_twilio", branch: "messageman"}
     ]
   end
 end
