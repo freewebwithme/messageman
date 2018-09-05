6ef074d24a63a0faf11e507d402df3f18fc02d40
Taehwan Kim <wlminimal@gmail.com>
2018-09-05 16:22:21 -0700

    Fix release_phone_number

--- lib/messageman/incoming_phone_number/incoming_phone_number.ex
+++ lib/messageman/incoming_phone_number/incoming_phone_number.ex
@@ -1,14 +1,14 @@
 defmodule Messageman.IncomingPhoneNumber do
-	alias ExTwilio.IncomingPhoneNumber
+	alias ExTwilio.IncomingPhoneNumber, as: ExIncomingPhoneNumber
 
 	def list_phone_numbers() do
-		IncomingPhoneNumber.all()
+		ExIncomingPhoneNumber.all()
 	end
 
 	def buy_phone_number(area_code, account, token) do
 		data = [AreaCode: area_code]
 		options = [account: account, token: token]
-		with {:ok, phone_number} <- IncomingPhoneNumber.create(data, options) do
+		with {:ok, phone_number} <- ExIncomingPhoneNumber.create(data, options) do
 			phone_number
 		else
 			{ :error, reason, http_status_code } -> { :error, reason, http_status_code }
@@ -18,10 +18,16 @@ defmodule Messageman.IncomingPhoneNumber do
 
 	def release_phone_number(phone_sid, account, token) do
 		options = [account: account, token: token]
-		with :ok <- IncomingPhoneNumber.destroy(phone_sid, options) do
-			{:ok, "Phone number is deleted from your account."}
-		else
-			{ :error, reason, http_status_code } -> { :error, reason, http_status_code }
+
+
+		case ExIncomingPhoneNumber.destroy(phone_sid, options) do
+			:ok -> {:ok, "Phone number is deleted from your account."}
+			{:error, reason, http_status_code} -> { :error, reason, http_status_code }
 		end
 	end
+
+	# def release_phone_number(phone_sid, account, token) do
+	# 	options = [account: account, token: token]
+	# 	IncomingPhoneNumber.destroy(phone_sid, options)
+	# end
 end
